1a4a43b5d9025d88ba4fc6f37a69933959ecfd52
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2024-10-27 04:37:54 -0600

    create main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Bienvenido</h1>
+        </div>
+      ),
+      errorElement: <div>Not Found 404</div>,
+      children: [],
+    },
+    {
+      path: "/signin",
+      element: <Signin />,
+      errorElement: <div>Not Found 404</div>,
+      children: [],
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+      errorElement: <div>Not Found 404</div>,
+      children: [],
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+      <Toaster />
+    </div>
+  );
 };
 
 export default App;
