356887581c825db02449fe4cf36396c6d14d60bc
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2024-11-12 12:55:19 -0600

    Create authentication context to handle user state

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+// context/AuthContext.js
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/config";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setCurrentUser(user));
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
